1afd335002c3283d3b805e29efe74e03c7d19284
Lukas Tassanyi <lukas.tassanyi@iota.org>
2019-02-01 12:54:57 +0100

    implement but uncomment (because worse score) attacker's strategy of passing every second round

--- src/main/java/org/iota/spamsim/nodes/MaliciousNode.java
+++ src/main/java/org/iota/spamsim/nodes/MaliciousNode.java
@@ -1,6 +1,7 @@
 package org.iota.spamsim.nodes;
 
 import org.iota.spamsim.Constants;
+import org.iota.spamsim.network.Transaction;
 import org.iota.spamsim.network.TransactionTransfer;
 
 public class MaliciousNode extends CfbNode {
@@ -13,4 +14,16 @@ public class MaliciousNode extends CfbNode {
     protected boolean acceptTransfer(TransactionTransfer transactionTransfer) {
         return false; // not broadcasting anyone else's transactions so I can broadcast as many of my own as possible
     }
+
+    /*
+    private boolean oddIteration = false;
+
+    @Override
+    public void emitNewTransactions() {
+        if(oddIteration) {
+            super.emitNewTransactions();
+            super.emitNewTransactions();
+        }
+        oddIteration = !oddIteration;
+    }*/
 }
